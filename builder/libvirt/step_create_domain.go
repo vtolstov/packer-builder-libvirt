d60a44a24daf371db081c3a43e760bafcd6715ac
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-07-27 17:58:55 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- builder/libvirt/step_create_domain.go
+++ builder/libvirt/step_create_domain.go
@@ -27,84 +27,92 @@ func (s *stepCreateDomain) Run(state multistep.StateBag) multistep.StepAction {
 		return multistep.ActionHalt
 	}
 	defer lv.CloseConnection()
-	if lvd, err := lv.LookupDomainByName(config.VMName); err != nil {
-
-		domainXml := bytes.NewBuffer(nil)
-		tmpl, err := template.New("domain").Parse(config.DomainXml)
+	if lvd, err := lv.LookupDomainByName(config.VMName); err == nil {
+		err = lvd.Destroy()
 		if err != nil {
-			err := fmt.Errorf("Error creating domain: %s", err)
+			err := fmt.Errorf("Error domain already running")
 			state.Put("error", err)
 			ui.Error(err.Error())
 			return multistep.ActionHalt
 		}
-
-		u, err := url.Parse(config.ISOUrl)
-		if err != nil {
-			err := fmt.Errorf("Error parse iso_url: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-		var h string
-		var p string
-
-		if strings.Index(u.Host, ":") == -1 {
-			h = u.Host
-		} else {
-			h, p, _ = net.SplitHostPort(u.Host)
-		}
-		if p == "" {
-			switch u.Scheme {
-			case "https":
-				p = "443"
-			case "http":
-				p = "80"
-			}
-		}
-
-		data := struct {
-			VMName      string
-			DiskName    string
-			DiskType    string
-			PoolName    string
-			MemorySize  uint
-			ISOUrlProto string
-			ISOUrlPath  string
-			ISOUrlHost  string
-			ISOUrlPort  string
-			SSHPort     string
-		}{
-			config.VMName,
-			config.DiskName,
-			"raw",
-			config.PoolName,
-			config.MemorySize,
-			u.Scheme,
-			u.Path,
-			h,
-			p,
-			"2022",
-		}
-		err = tmpl.Execute(domainXml, data)
-		if err != nil {
-			err := fmt.Errorf("Error creating domain: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-
-		ui.Say(fmt.Sprintf("domain config:\n%s", domainXml.Bytes()))
-
-		lvd, err = lv.DomainCreateXML(string(domainXml.Bytes()), 0)
-		if err != nil {
-			err := fmt.Errorf("Error creating domain: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-	} else {
-		defer lvd.Free()
 	}
+
+	domainXml := bytes.NewBuffer(nil)
+	tmpl, err := template.New("domain").Parse(config.DomainXml)
+	if err != nil {
+		err := fmt.Errorf("Error creating domain: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	u, err := url.Parse(config.ISOUrl)
+	if err != nil {
+		err := fmt.Errorf("Error parse iso_url: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	var h string
+	var p string
+
+	if strings.Index(u.Host, ":") == -1 {
+		h = u.Host
+	} else {
+		h, p, _ = net.SplitHostPort(u.Host)
+	}
+	if p == "" {
+		switch u.Scheme {
+		case "https":
+			p = "443"
+		case "http":
+			p = "80"
+		}
+	}
+
+	data := struct {
+		VMName      string
+		DiskName    string
+		DiskType    string
+		PoolName    string
+		MemorySize  uint
+		ISOUrlProto string
+		ISOUrlPath  string
+		ISOUrlHost  string
+		ISOUrlPort  string
+		SSHPort     string
+	}{
+		config.VMName,
+		config.DiskName,
+		"raw",
+		config.PoolName,
+		config.MemorySize,
+		u.Scheme,
+		u.Path,
+		h,
+		p,
+		"2022",
+	}
+	err = tmpl.Execute(domainXml, data)
+	if err != nil {
+		err := fmt.Errorf("Error creating domain: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	ui.Say(fmt.Sprintf("domain config:\n%s", domainXml.Bytes()))
+
+	lvd, err := lv.DomainCreateXML(string(domainXml.Bytes()), 0)
+	if err != nil {
+		err := fmt.Errorf("Error creating domain: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	defer lvd.Free()
+
 	return multistep.ActionContinue
 }
 
