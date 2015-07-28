8dc24a48c4629806159eb84898653c7cd1dbe49b
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-07-28 17:59:32 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- builder/libvirt/step_type_boot_command.go
+++ builder/libvirt/step_type_boot_command.go
@@ -89,7 +89,7 @@ func sendBootString(d libvirt.VirDomain, original string) {
 	var key uint
 
 	for len(original) > 0 {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(50 * time.Millisecond)
 		if strings.HasPrefix(original, "<wait>") {
 			log.Printf("Special code '<wait>' found, sleeping one second")
 			time.Sleep(1 * time.Second)
@@ -112,13 +112,13 @@ func sendBootString(d libvirt.VirDomain, original string) {
 		}
 
 		if strings.HasPrefix(original, "<esc>") {
-			d.SendKey(libvirt.VIR_KEYCODE_SET_RFB, 100, []uint{ecodes["<esc>"]}, 0)
+			d.SendKey(libvirt.VIR_KEYCODE_SET_RFB, 50, []uint{ecodes["<esc>"]}, 0)
 			original = original[len("<esc>"):]
 			continue
 		}
 
 		if strings.HasPrefix(original, "<enter>") {
-			d.SendKey(libvirt.VIR_KEYCODE_SET_RFB, 100, []uint{ecodes["<enter>"]}, 0)
+			d.SendKey(libvirt.VIR_KEYCODE_SET_RFB, 50, []uint{ecodes["<enter>"]}, 0)
 			original = original[len("<enter>"):]
 			continue
 		}
@@ -129,7 +129,7 @@ func sendBootString(d libvirt.VirDomain, original string) {
 		key = ecodes[string(r)]
 		log.Printf("find code for char %s %d", string(r), key)
 		//VIR_KEYCODE_SET_LINUX, VIR_KEYCODE_SET_USB, VIR_KEYCODE_SET_RFB, VIR_KEYCODE_SET_WIN32, VIR_KEYCODE_SET_XT_KBD
-		if err = d.SendKey(libvirt.VIR_KEYCODE_SET_RFB, 100, []uint{key}, 0); err != nil {
+		if err = d.SendKey(libvirt.VIR_KEYCODE_SET_RFB, 50, []uint{key}, 0); err != nil {
 			log.Printf("Sending code %d failed: %s", key, err.Error())
 		}
 	}
